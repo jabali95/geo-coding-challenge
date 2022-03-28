f24f6cadbdbf7e6140e1378e2485b8ae4017936a
Jabali95 <jabaliahmad4@gmail.com>
2022-03-28 23:46:01 +0200

    complete assessment

--- src/geofence.ts
+++ src/geofence.ts
@@ -1,7 +1,60 @@
-import { GeofenceInterface } from './geofence-interface';
+import { FeatureCollection, Polygon, Position } from "@vpriem/geojson";
+import { GeofenceInterface } from "./geofence-interface";
 
 export class Geofence implements GeofenceInterface {
-    /**
-     * TODO: implement interface
-     */
+  data: FeatureCollection<Polygon> | undefined;
+
+  init(data: FeatureCollection<Polygon>): Promise<void> {
+    this.data = data;
+    return Promise.resolve();
+  }
+
+  shutdown(): Promise<void> {
+    delete this.data;
+    return Promise.resolve();
+  }
+
+  set(position: Position): Promise<FeatureCollection<Polygon>> {
+
+    if (!this.data) {
+      return Promise.reject("data is undefined");
+    }
+
+    try {
+      return Promise.resolve({
+        type: "FeatureCollection",
+        features: this.data.features.filter((polygon) =>
+          this.rayTracing_oddEvenRule(
+            position[0],
+            position[1],
+            polygon.geometry
+          )
+        ),
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  rayTracing_oddEvenRule = function (
+    positionLong: number,
+    positionLat: number,
+    polygon: Polygon
+  ) {
+    let odd = false;
+    var aX: number, bX: number, aY: number, bY: number;
+    for (let i = 0; i < polygon.coordinates.length; i++) {
+      for (let j = 0; j < polygon.coordinates[i].length - 1; j++) {
+        aX = polygon.coordinates[i][j][0];
+        bX = polygon.coordinates[i][j + 1][0];
+        aY = polygon.coordinates[i][j][1];
+        bY = polygon.coordinates[i][j + 1][1];
+
+        if (positionLat < aY != positionLat < bY && positionLong < ((bX - aX) * (positionLat - aY)) / (bY - aY) + aX)
+          odd = !odd;
+      }
+    }
+    //If the number of crossings was odd, the point is in the polygon
+    return odd;
+  };
 }
--- src/geosearch.ts
+++ src/geosearch.ts
@@ -1,7 +1,86 @@
+import { Position, Polygon, FeatureCollection, Point } from '@vpriem/geojson';
 import { GeosearchInterface } from './geosearch-interface';
+import booleanIntersects from '@turf/boolean-intersects';
+import circle from '@turf/circle';
 
 export class Geosearch implements GeosearchInterface {
-    /**
-     * TODO: implement interface
-     */
+    data: FeatureCollection<Polygon | Point> | undefined;
+
+    init(data: FeatureCollection<Polygon | Point>): Promise<void> {
+        this.data = data;
+        return Promise.resolve();
+    }
+
+    shutdown(): Promise<void> {
+        delete this.data;
+        return Promise.resolve();
+    }
+
+    find(
+        position: Position,
+        radius: number
+    ): Promise<FeatureCollection<Polygon | Point>> {
+
+        if (!this.data) {
+            return Promise.reject("data is undefined");
+        }
+        try {
+            return Promise.resolve({
+                type: 'FeatureCollection',
+                features: this.data.features.filter((feature) => {
+                    return this.isInSearchArea(
+                        radius,
+                        position,
+                        feature.geometry,
+                    );
+                }),
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
+
+    }
+
+    isInSearchArea = function (
+        radius: number,
+        position: Position,
+        geometry: Polygon | Point
+    ) {
+        if (geometry.type === 'Point') {
+
+            var givenPointX: number = geometry.coordinates[0];
+            var givenPointY: number = geometry.coordinates[1];
+            var circleX: number = position[0];
+            var circleY: number = position[1];
+            return this.isWithinCircleRadius_haversine(givenPointX, givenPointY, circleX, circleY, radius);
+
+        } else if (geometry.type === 'Polygon') {
+
+            var createdCircle = circle(position, radius, {
+                units: 'meters',
+            });
+            return booleanIntersects(createdCircle, geometry);
+
+        }
+    };
+
+    isWithinCircleRadius_haversine = function (a: number, b: number, x: number, y: number, radius: number) {
+
+        var earthRadius: number = 6371000;
+        var dLat: number = this.toRad(y - b);
+        var dLon: number = this.toRad(x - a);
+        var lat1: number = this.toRad(y);
+        var lat2: number = this.toRad(b);
+
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = earthRadius * c;
+
+        return d <= radius;
+    };
+
+    toRad = function (value: number) {
+        return value * Math.PI / 180;
+    };
 }
